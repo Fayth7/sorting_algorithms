80a92e34c07d6227c03a047604b951cbc251f288
Fayth7 <flexflakes@gmail.com>
2023-06-24 11:27:57 -0700

    ipdate

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-    (*n1)->next = n2->next;
-    if (n2->next != NULL)
-        n2->next->prev = *n1;
-    n2->prev = (*n1)->prev;
-    n2->next = *n1;
-    if ((*n1)->prev != NULL)
-        (*n1)->prev->next = n2;
-    else
-        *h = n2;
-    (*n1)->prev = n2;
-    *n1 = n2->prev;
-}
-
-/**
  * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
+ *                       in ascending order using Insertion sort algorithm.
+ * @list: A pointer to the head of the doubly linked list.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *iter, *insert, *tmp;
-
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+ listint_t *sorted;
+    listint_t *unsorted;
+    listint_t *curr;
+    listint_t *temp;
+    
+  if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    for (iter = (*list)->next; iter != NULL; iter = tmp)
+    sorted = *list;
+    unsorted = (*list)->next;
+
+    while (unsorted != NULL)
     {
-        tmp = iter->next;
-        insert = iter->prev;
-        while (insert != NULL && iter->n < insert->n)
+        curr = unsorted;
+        unsorted = unsorted->next;
+
+        while (curr->prev != NULL && curr->n < curr->prev->n)
         {
-            swap_nodes(list, &insert, iter);
-            print_list((const listint_t *)*list);
+            temp = curr->prev;
+            temp->next = curr->next;
+
+            if (curr->next != NULL)
+                curr->next->prev = temp;
+
+            curr->prev = temp->prev;
+            curr->next = temp;
+            temp->prev = curr;
+
+            if (curr->prev != NULL)
+                curr->prev->next = curr;
+            else
+                sorted = curr;
+
+            print_list(*list);
         }
     }
+
+    *list = sorted;
 }
