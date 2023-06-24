24cb2765b25f385b76ebdd03c502caf83af8035e
Fayth7 <flexflakes@gmail.com>
2023-06-24 12:15:34 -0700

    shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,34 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using the
- *              Shell sort algorithm (Knuth sequence).
- * @array: The array to be sorted.
- * @size: The size of the array.
+ * shell_sort - Sorts an array of integers in ascending order using the Shell sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
  */
 void shell_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+size_t gap = 1, i, j;
+int temp;
 
-    size_t gap;
-    gap = 1;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+while (gap < size / 3)
+gap = gap * 3 + 1;
 
-    while (gap >= 1)
-    {
-        for (i = gap; i < size; i++)
-        {
-            int temp = array[i];
-            size_t j = i;
+while (gap >= 1)
+{
+for (i = gap; i < size; i++)
+{
+temp = array[i];
 
-            while (j >= gap && array[j - gap] > temp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
+for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+array[j] = array[j - gap];
 
-            array[j] = temp;
-        }
+array[j] = temp;
+}
 
-        print_array(array, size);
+print_array(array, size);
 
-        gap /= 3;
-    }
+gap /= 3;
+}
 }
