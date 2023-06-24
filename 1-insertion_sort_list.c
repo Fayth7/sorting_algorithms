07489feb55aea9d1402d991f8a99d35547989f51
Fayth7 <flexflakes@gmail.com>
2023-06-24 11:11:03 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- *                       using the Insertion sort algorithm.
- * @list: Pointer to a pointer to the head of the list.
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+    (*n1)->next = n2->next;
+    if (n2->next != NULL)
+        n2->next->prev = *n1;
+    n2->prev = (*n1)->prev;
+    n2->next = *n1;
+    if ((*n1)->prev != NULL)
+        (*n1)->prev->next = n2;
+    else
+        *h = n2;
+    (*n1)->prev = n2;
+    *n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *iter, *insert, *tmp;
+
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *current;
-    current = (*list)->next;
-
-    while (current != NULL)
+    for (iter = (*list)->next; iter != NULL; iter = tmp)
     {
-        listint_t *temp = current->prev;
-        int value = current->n;
-
-        while (temp != NULL && temp->n > value)
+        tmp = iter->next;
+        insert = iter->prev;
+        while (insert != NULL && iter->n < insert->n)
         {
-            temp->next->n = temp->n;
-            temp = temp->prev;
+            swap_nodes(list, &insert, iter);
+            print_list((const listint_t *)*list);
         }
-
-        if (temp == NULL)
-            (*list)->n = value;
-        else
-            temp->next->n = value;
-
-        print_list(*list);
-        current = current->next;
     }
 }
